50059c9187d3a29963b38378928633bd11edc603
creamynald <creamynald@gmail.com>
2024-05-30 18:54:21 +0700

    fix seeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -19,40 +19,92 @@ class PermissionSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         // create permissions
-        Permission::create(['name' => 'edit articles']);
-        Permission::create(['name' => 'delete articles']);
-        Permission::create(['name' => 'publish articles']);
-        Permission::create(['name' => 'unpublish articles']);
+        Permission::create(['name' => 'lihat.buku']);
+        Permission::create(['name' => 'pinjam.buku']);
+        Permission::create(['name' => 'delete.buku']);
+        Permission::create(['name' => 'edit.buku']);
+
+        Permission::create(['name' => 'lihat.anggota']);
+        Permission::create(['name' => 'input.anggota']);
+        Permission::create(['name' => 'delete.anggota']);
+        Permission::create(['name' => 'edit.anggota']);
+        
+        Permission::create(['name' => 'lihat.peminjaman']);
+        Permission::create(['name' => 'input.peminjaman']);
+        Permission::create(['name' => 'delete.peminjaman']);
+        Permission::create(['name' => 'edit.peminjaman']);
+
+        Permission::create(['name' => 'lihat.pengembalian']);
+        Permission::create(['name' => 'input.pengembalian']);
+        Permission::create(['name' => 'delete.pengembalian']);
+        Permission::create(['name' => 'edit.pengembalian']);
+
+        Permission::create(['name' => 'lihat.laporan']);
+        Permission::create(['name' => 'input.laporan']);
+        Permission::create(['name' => 'delete.laporan']);
+        Permission::create(['name' => 'edit.laporan']);
 
         // create roles and assign existing permissions
-        $role1 = Role::create(['name' => 'writer']);
-        $role1->givePermissionTo('edit articles');
-        $role1->givePermissionTo('delete articles');
+        $role1 = Role::create(['name' => 'siswa']);
+        $role1->givePermissionTo('lihat.buku');
+        $role1->givePermissionTo('pinjam.buku');
+        $role1->givePermissionTo('lihat.anggota');
+        $role1->givePermissionTo('lihat.peminjaman');
+        $role1->givePermissionTo('lihat.pengembalian');
 
         $role2 = Role::create(['name' => 'admin']);
-        $role2->givePermissionTo('publish articles');
-        $role2->givePermissionTo('unpublish articles');
+        $role2->givePermissionTo('lihat.buku');
+        $role2->givePermissionTo('pinjam.buku');
+        $role2->givePermissionTo('delete.buku');
+        $role2->givePermissionTo('edit.buku');
+        $role2->givePermissionTo('lihat.anggota');
+        $role2->givePermissionTo('input.anggota');
+        $role2->givePermissionTo('delete.anggota');
+        $role2->givePermissionTo('edit.anggota');
+        $role2->givePermissionTo('lihat.peminjaman');
+        $role2->givePermissionTo('input.peminjaman');
+        $role2->givePermissionTo('delete.peminjaman');
+        $role2->givePermissionTo('edit.peminjaman');
+        $role2->givePermissionTo('lihat.pengembalian');
+        $role2->givePermissionTo('input.pengembalian');
+        $role2->givePermissionTo('delete.pengembalian');
+        $role2->givePermissionTo('edit.pengembalian');
+        $role2->givePermissionTo('lihat.laporan');
+        $role2->givePermissionTo('input.laporan');
+        $role2->givePermissionTo('delete.laporan');
+        $role2->givePermissionTo('edit.laporan');
 
-        $role3 = Role::create(['name' => 'super admin']);
+        $role3 = Role::create(['name' => 'kepsek']);
+        $role3->givePermissionTo('lihat.peminjaman');
+        $role3->givePermissionTo('lihat.laporan');
+        
+
+        $role4 = Role::create(['name' => 'super admin']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
         // create demo users
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Example User',
-            'email' => 'test@example.com',
+        $siswa = \App\Models\User::factory()->create([
+            'name' => 'Siswa',
+            'email' => 'siswa@perpus.test',
         ]);
-        $user->assignRole($role1);
+        $siswa->assignRole($role1);
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Example Admin User',
-            'email' => 'admin@example.com',
+        $operator = \App\Models\User::factory()->create([
+            'name' => 'Admin Perpus',
+            'email' => 'admin@perpus.test',
         ]);
-        $user->assignRole($role2);
+        $operator->assignRole($role2);
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Example Super-Admin User',
-            'email' => 'superadmin@example.com',
+        $kepsek = \App\Models\User::factory()->create([
+            'name' => 'Kepala Sekolah',
+            'email' => 'kepsek@perpus.test',
         ]);
-        $user->assignRole($role3);
+        $kepsek->assignRole($role3);
+
+        $superadmin = \App\Models\User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@perpus.test',
+        ]);
+        $superadmin->assignRole($role4);
     }
 }
