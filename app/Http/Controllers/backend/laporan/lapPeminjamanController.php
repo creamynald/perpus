3e2f608fe5673be30bffa269dc51dfd5ff202651
creamynald <creamynald@gmail.com>
2024-07-29 09:00:58 +0700

    fix: laporan peminjaman

--- app/Http/Controllers/backend/laporan/lapPeminjamanController.php
+++ app/Http/Controllers/backend/laporan/lapPeminjamanController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\backend\laporan;
 
 use App\Http\Controllers\Controller;
+use App\Models\Peminjaman;
 use Illuminate\Http\Request;
 
 class lapPeminjamanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('backend.laporan.lapPeminjaman.index');
+        $peminjaman = collect();
+
+        if ($request->has('tgl_awal') && $request->has('tgl_akhir')) {
+            $peminjaman = Peminjaman::whereBetween('tanggal_pinjam', [$request->tgl_awal, $request->tgl_akhir])->get();
+        }
+
+        return view('backend.laporan.lapPeminjaman.index', compact('peminjaman'));
     }
 }
--- resources/views/backend/laporan/lapPeminjaman/index.blade.php
+++ resources/views/backend/laporan/lapPeminjaman/index.blade.php
@@ -15,10 +15,10 @@
                 <div class="block-options">
                     <form action="{{ route('lap-peminjaman.index') }}" method="get">
                         <div class="input-group">
-                            <input type="date" class="form-control" name="tgl_awal" value="#">
-                            <input type="date" class="form-control" name="tgl_akhir" value="#">
+                            <input type="date" class="form-control" name="tgl_awal" value="{{ request('tgl_awal') }}">
+                            <input type="date" class="form-control" name="tgl_akhir" value="{{ request('tgl_akhir') }}">
                             <button type="submit" class="btn btn-sm btn-primary">
-                                <i class="fa fa-search opacity-50"></i> Terapkan Filter
+                                <i class="fa fa-search opacity-50"></i> Tampilkan
                             </button>
                         </div>
                     </form>
@@ -30,54 +30,73 @@
                 </div>
             </div>
         </div>
-        <div class="block-content block-content-full">
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
-                    <thead>
-                        <tr>
-                            <th class="text-center">No</th>
-                            <th class="text-center">Kode Peminjaman</th>
-                            <th class="text-center">Nama Anggota</th>
-                            <th class="text-center">Judul Buku</th>
-                            <th class="text-center">Tanggal Pinjam</th>
-                            <th class="text-center">Tanggal Kembali</th>
-                            <th class="text-center">Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    </tbody>
-                </table>
+        @if ($peminjaman->isNotEmpty())
+            <div class="block-content block-content-full">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
+                        <thead>
+                            <tr>
+                                <th class="text-center">No</th>
+                                <th class="text-center">Kode Peminjaman</th>
+                                <th class="text-center">Nama Anggota</th>
+                                <th class="text-center">Judul Buku</th>
+                                <th class="text-center">Tanggal Pinjam</th>
+                                <th class="text-center">Tanggal Kembali</th>
+                                <th class="text-center">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($peminjaman as $key => $row)
+                                <tr>
+                                    <td class="text-center">{{ $key + 1 }}</td>
+                                    <td class="text-center">{{ $row->kode_peminjaman }}</td>
+                                    <td class="text-center">{{ $row->user->name }}</td>
+                                    <td class="text-center">{{ $row->pustaka->judul_pustaka }}</td>
+                                    <td class="text-center">{{ $row->tanggal_pinjam }}</td>
+                                    <td class="text-center">{{ $row->tanggal_kembali }}</td>
+                                    <td class="text-center">{{ $row->status }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <!-- END Dynamic Table Full -->
-        </div>
-    @endsection
+        @else
+            <div class="block-content block-content-full">
+                <div class="alert alert-warning" role="alert">
+                    Tidak ada data untuk ditampilkan. Silakan terapkan filter untuk melihat data.
+                </div>
+            </div>
+        @endif
+        <!-- END Dynamic Table Full -->
+    </div>
+@endsection
 
-    @push('css')
-        <!-- Page JS Plugins CSS -->
-        <link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables-bs5/css/dataTables.bootstrap5.min.css') }}">
-        <link rel="stylesheet"
-            href="{{ asset('assets/js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}">
-        <link rel="stylesheet"
-            href="{{ asset('assets/js/plugins/datatables-responsive-bs5/css/responsive.bootstrap5.min.css') }}">
-    @endpush
+@push('css')
+    <!-- Page JS Plugins CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables-bs5/css/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/js/plugins/datatables-buttons-bs5/css/buttons.bootstrap5.min.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('assets/js/plugins/datatables-responsive-bs5/css/responsive.bootstrap5.min.css') }}">
+@endpush
 
-    @push('js')
-        <!-- jQuery (required for DataTables plugin) -->
-        <script src="{{ asset('assets/js/lib/jquery.min.js') }}"></script>
+@push('js')
+    <!-- jQuery (required for DataTables plugin) -->
+    <script src="{{ asset('assets/js/lib/jquery.min.js') }}"></script>
 
-        <!-- Page JS Plugins -->
-        <script src="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons/dataTables.buttons.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons-jszip/jszip.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/pdfmake.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/vfs_fonts.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.print.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
+    <!-- Page JS Plugins -->
+    <script src="{{ asset('assets/js/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-bs5/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-responsive-bs5/js/responsive.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-bs5/js/buttons.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons-pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins/datatables-buttons/buttons.html5.min.js') }}"></script>
 
-        <!-- Page JS Code -->
-        <script src="{{ asset('assets/js/pages/be_tables_datatables.min.js') }}"></script>
-    @endpush
+    <!-- Page JS Code -->
+    <script src="{{ asset('assets/js/pages/be_tables_datatables.min.js') }}"></script>
+@endpush
